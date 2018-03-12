2bbce92cd6186dae72e5ed56b7f42f40b5c2d9dd
Владислав Павлов <vlpavlov58@gmail.com>
2018-03-12 23:07:04 +0200

    Задача 10.5   Написать две функции, которые возвращают соответственно минимум   и максимум из двух чисел, которые передаются в функцию в качестве   аргументов.

--- HomeWork_10_5/Program.cs
+++ HomeWork_10_5/Program.cs
@@ -8,8 +8,35 @@ namespace HomeWork_10_5
 {
     class Program
     {
+        static int GetMin(int x, int y)
+        {
+            
+            if (x>y)
+            {
+                Console.WriteLine("Min: "+y); 
+            }
+            return y;
+        }
+        static int GetMax(int x, int y)
+        {
+
+            if (x < y)
+            {
+                Console.WriteLine("Max: " + y);
+            }
+            else if (x==y)
+            {
+                Console.WriteLine(x +" Equals " + y);
+            }
+            return y;
+        }
+
         static void Main(string[] args)
         {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            GetMin(x, y);
+            GetMax(x, y);
         }
     }
 }
